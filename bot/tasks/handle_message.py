908f30eb1bc77c6373a449f111fd5d644e9156f5
David Davidović <geosoft.corp@gmail.com>
2017-02-21 01:16:02 +0100

    Remove debug

--- bot/tasks/handle_message.py
+++ bot/tasks/handle_message.py
@@ -3,5 +3,4 @@ from .send_message import send_text_message
 
 @app.task
 def handle_message(sender_id, body):
-    print("Handling message {0}".format(body))
     send_text_message.delay(sender_id, "Alo, 555-333?")
