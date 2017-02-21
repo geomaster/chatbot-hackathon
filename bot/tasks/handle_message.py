458fdd71a4bb1df38d893ffa884fb8c9ef4e74f3
David Davidović <geosoft.corp@gmail.com>
2017-02-21 03:10:58 +0100

    revert bypass

--- bot/tasks/handle_message.py
+++ bot/tasks/handle_message.py
@@ -4,5 +4,4 @@ from .send_message import send_text_message
 
 @app.task
 def handle_message(sender_id, msg):
-    send_text_message.delay(sender_id, "wit bypass test")
-    #return handle_via_wit(sender_id, msg.get("text"))
+    return handle_via_wit(sender_id, msg.get("text"))
