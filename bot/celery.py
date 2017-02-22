aa70be864f90585c16a7dc3eed0c3bbe861de357
David Davidović <geosoft.corp@gmail.com>
2017-02-22 13:32:59 +0100

    Add celery speed

--- bot/celery.py
+++ bot/celery.py
@@ -1,9 +1,30 @@
 from celery import Celery
 
+task_routes = {
+    'bot.tasks.handle_message.handle_message': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_action': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_attachment': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_attachment_url': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_audio': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_audio_url': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_button_message': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_file': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_file_url': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_generic_message': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_image': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_image_url': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_message': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_raw': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_text_message': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_video': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_message.send_video_url': {'queue': 'celery', 'delivery_mode': 'transient'},
+    'bot.tasks.send_text_message': {'queue': 'celery', 'delivery_mode': 'transient'}
+}
+
 celery_app = Celery("bot",
                     broker="amqp://localhost",
-                    backend="amqp://localhost",
-                    include=["bot.tasks"])
+                    include=["bot.tasks"],
+                    task_routes=task_routes)
 
 if __name__ == "__main__":
     celery_app.start()
