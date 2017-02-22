4c1db405db06e3d09b1a389def6dc6c42daebd56
David Davidović <geosoft.corp@gmail.com>
2017-02-22 14:17:01 +0100

    ignore_result

--- bot/tasks/handle_message.py
+++ bot/tasks/handle_message.py
@@ -3,7 +3,7 @@ from ..wit import handle_via_wit
 from . import send_message
 import time
 
-@app.task
+@app.task(ignore_result=True)
 def handle_message(sender_id, msg):
     print("[{0}] Handling message at {1}".format(msg["mid"], time.time()))
     handle_via_wit(sender_id, msg.get("text"), send_message)
--- bot/tasks/send_message.py
+++ bot/tasks/send_message.py
@@ -5,7 +5,7 @@ import time
 
 bot = Bot(ACCESS_TOKEN, app_secret=APP_SECRET)
 
-@app.task
+@app.task(ignore_result=True)
 def send_text_message(recipient_id, message, quickreplies=None):
     print("[{0}] Sending message at {1}".format(recipient_id, time.time()))
     if quickreplies:
@@ -15,62 +15,62 @@ def send_text_message(recipient_id, message, quickreplies=None):
         bot.send_text_message(recipient_id, message)
     print("[{0}] Sent message at {1}".format(recipient_id, time.time()))
 
-@app.task
+@app.task(ignore_result=True)
 def send_message(recipient_id, message):
     bot.send_message(recipient_id, message)
 
-@app.task
+@app.task(ignore_result=True)
 def send_generic_message(recipient_id, elements):
     bot.send_generic_message(recipient_id, elements)
 
-@app.task
+@app.task(ignore_result=True)
 def send_button_message(recipient_id, text, buttons):
     bot.send_button_message(recipient_id, text, buttons)
 
-@app.task
+@app.task(ignore_result=True)
 def send_attachment(recipient_id, attachment_type, attachment_path):
     bot.send_attachment(recipient_id, attachment_type, attachment_path)
 
-@app.task
+@app.task(ignore_result=True)
 def send_attachment_url(recipient_id, attachment_type, attachment_url):
     bot.send_attachment_url(recipient_id, attachment_type, attachment_url)
 
-@app.task
+@app.task(ignore_result=True)
 def send_image(recipient_id, image_path):
     bot.send_image(recipient_id, image_path)
 
-@app.task
+@app.task(ignore_result=True)
 def send_image_url(recipient_id, image_url):
     bot.send_image_url(recipient_id, image_url)
 
-@app.task
+@app.task(ignore_result=True)
 def send_audio(recipient_id, audio_path):
     bot.send_audio(recipient_id, audio_path)
 
-@app.task
+@app.task(ignore_result=True)
 def send_audio_url(recipient_id, audio_url):
     bot.send_audio_url(recipient_id, audio_url)
 
-@app.task
+@app.task(ignore_result=True)
 def send_video(recipient_id, video_path):
     bot.send_video(recipient_id, video_path)
 
-@app.task
+@app.task(ignore_result=True)
 def send_video_url(recipient_id, video_url):
     bot.send_video_url(recipient_id, video_url)
 
-@app.task
+@app.task(ignore_result=True)
 def send_file(recipient_id, file_path):
     bot.send_file(recipient_id, file_path)
 
-@app.task
+@app.task(ignore_result=True)
 def send_file_url(recipient_id, file_url):
     bot.send_file_url(recipient_id, file_url)
 
-@app.task
+@app.task(ignore_result=True)
 def send_action(recipient_id, action):
     bot.send_action(recipient_id, action)
 
-@app.task
+@app.task(ignore_result=True)
 def send_raw(payload):
     bot.send_raw(payload)
