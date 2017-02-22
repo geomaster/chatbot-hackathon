f45026eb7b79af805ab7b339529a160e4efe1817
David Davidović <geosoft.corp@gmail.com>
2017-02-22 14:20:05 +0100

    whoops

--- bot/tasks/handle_message.py
+++ bot/tasks/handle_message.py
@@ -6,5 +6,5 @@ import time
 @app.task(ignore_result=True)
 def handle_message(sender_id, msg):
     print("[{0}] Handling message at {1}".format(msg["mid"], time.time()))
-    send_text_message.delay(sender_id, "wit_bypass_test")
+    send_message.send_text_message.delay(sender_id, "wit_bypass_test")
     #handle_via_wit(sender_id, msg.get("text"), send_message)
