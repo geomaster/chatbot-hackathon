0c474e0fc8649343a6d405f81fa9d989592ea437
David Davidović <geosoft.corp@gmail.com>
2017-02-25 18:35:27 +0100

    Modify server ``

--- server/app.py
+++ server/app.py
@@ -52,7 +52,6 @@ def dashboard_questions():
 def dashboard_surveys():
     return render_template("surveys.html")
 
-
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(host="127.0.0.1", port=9889)
